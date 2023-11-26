be0f49c222a441325c796f9fc0a5cb7e3ba07f4a
Sean <34967@ma-web.nl>
2023-11-26 23:03:56 +0100

    08 select

---
+++ m6prog_project/docker/public/assets/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once '../source/config.php';
+
+require_once SOURCE_ROOT . 'database.php';
+
+$connection = database_connect();
+$plaats = 'amsterdam';
+$sql = 'SELECT * FROM weersomstandighedenPerDag WHERE Plaats=? ORDER BY Datum'; 
+$stmt = $connection->prepare($sql);
+$stmt->bind_param('s', 'amsterdam');
+$stmt->execute();
+$result = $stmt->get_result();
+$weersomstandigheden = mysqli_fetch_assoc($result);
+var_dump( $weersomstandigheden );
