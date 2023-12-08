cac8c36f8b6206dc8dc93120b5f63ca6a77a2fe6
Sean <34967@ma-web.nl>
2023-12-08 12:14:36 +0100

    04 superglobal

--- m6prog_project/public/nawOpslaan.php
+++ m6prog_project/public/nawOpslaan.php
@@ -1,3 +1,52 @@
 <?php
-        var_dump( $_POST );
+        $naam = '';
+        $straat = '';
+        $huisnummer = '';
+        $postcode = '';
+        $emailAdres = '';
+        
+        echo '<b>naam: </b>';
+        if ( empty( $_POST['naam']) ) {
+            echo '<b style="color:#f00;">Je moet wel je naam invullen</b>';
+        } else {
+            $naam = $_POST['naam'];
+        }
+        echo $naam;
+        echo '<br>';
+
+        echo '<b>straat: </b>';
+        if ( empty( $_POST['straat']) ) {
+            echo '<b style="color:#f00;">Je moet wel je straat invullen</b>';
+        } else {
+            $straat = $_POST['straat'];
+        }
+        echo $straat;
+        echo '<br>';
+
+        echo '<b>huisnummer: </b>';
+        if ( empty( $_POST['huisnummer']) ) {
+            echo '<b style="color:#f00;">Je moet wel je huisnummer invullen</b>';
+        } else {
+            $huisnummer = $_POST['huisnummer'];
+        }
+        echo $huisnummer;
+        echo '<br>';
+
+        echo '<b>postcode: </b>';
+        if ( empty( $_POST['postcode']) ) {
+            echo '<b style="color:#f00;">Je moet wel je postcode invullen</b>';
+        } else {
+            $postcode = $_POST['postcode'];
+        }
+        echo $postcode;
+        echo '<br>';
+
+        echo '<b>email Adres: </b>';
+        if ( empty( $_POST['emailAdres']) ) {
+            echo '<b style="color:#f00;">Je moet wel je email adres invullen</b>';
+        } else {
+            $emailAdres = $_POST['emailAdres'];
+        }
+        echo $emailAdres;
+        echo '<br>';
 ?>
