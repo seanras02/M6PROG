55aa4d33e6489d645869cb8273d0dc0d7adf442f
Sean <34967@ma-web.nl>
2023-12-14 02:39:08 +0100

    04 post versturen

--- m6prog_project/public/assets/js/appFetch.js
+++ m6prog_project/public/assets/js/appFetch.js
@@ -1,3 +1,39 @@
+let postForm = document.getElementById("postForm");
+
+postForm.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    toPhpWithPost(event);
+});
+
+function toPhpWithPost(event){
+    let form = event.target;
+    let jsonForm = FormToDictionary(form);
+    console.log(jsonForm);
+
+    let options = {
+        method: "POST",
+        cache: "no-cache",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(jsonForm)
+    }
+
+    fetch("fetchPost.php", options)
+    .then(async (response)=>{
+        console.log(response);
+    });
+}
+
+
+
+function FormToDictionary(form){
+    const data = new FormData(form);
+    let formKeyValue={};
+    for (const [name, value] of data){
+        formKeyValue[name] = value;
+    }
+    return formKeyValue;
+}
+
 
 let getForm = document.getElementById("getForm");
 
@@ -6,7 +42,6 @@ getForm.addEventListener("submit", (event)=>
     event.preventDefault();
     toPhpWithGet(event);
 });
-
 
 function toPhpWithGet(event){
     let form = event.target;
