a2b66741e71f2b01dc962ad7a51f61c3cc78315b
Sean <34967@ma-web.nl>
2024-01-29 15:24:44 +0100

    03 delete

---
+++ m6prog_project/sql/03 delete proberen.sql
@@ -0,0 +1,3 @@
+DELETE FROM image where id =1;
+
+DELETE FROM image where Link ="http";
---
+++ m6prog_project/sql/03 image insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `bo`.`image` (`FileType`, `FileSize`, `Link`, `FileID`, `uploaddatetime`) VALUES ('png', '4000', 'http', 'kuyghgv', CURRENT_TIMESTAMP);
+INSERT INTO `bo`.`image` (`FileType`, `FileSize`, `Link`, `FileID`, `uploaddatetime`) VALUES ('png', '2000', 'http', 'ewjfge', CURRENT_TIMESTAMP);
+INSERT INTO `bo`.`image` (`FileType`, `FileSize`, `Link`, `FileID`, `uploaddatetime`) VALUES ('png', '5000', 'http', 'ergfjwere', CURRENT_TIMESTAMP);
+INSERT INTO `bo`.`image` (`FileType`, `FileSize`, `Link`, `FileID`, `uploaddatetime`) VALUES ('png', '1000', 'http', 'e324ferv', CURRENT_TIMESTAMP);
